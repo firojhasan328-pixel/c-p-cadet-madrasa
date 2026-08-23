774c5052c789182608b51d0faddfeafdc3732050
firojhasan328-pixel <firojhasan328@gmail.com>
2026-08-23 13:07:56 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// ❌ AuthContext ডিলিট করা হয়েছে
-// ✅ এখন App নিজেই PortalProvider ব্যবহার করছে
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
