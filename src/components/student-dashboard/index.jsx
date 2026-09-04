8c6dbbeb920ad7a9d0fee2f362a7607b34c475a5
firojhasan328-pixel <firojhasan328@gmail.com>
2026-09-04 16:06:39 +0600

    Update index.jsx

--- src/components/student-dashboard/index.jsx
+++ src/components/student-dashboard/index.jsx
@@ -1,5 +1,5 @@
 export { default as StudentDashboard } from './StudentDashboard';
 export { default as DashboardCards } from './DashboardCards';
 export { default as ProfileCard } from './ProfileCard';
-export { default as NoticeCard } from './NoticeCard';
 export { default as QuickStats } from './QuickStats';
+export { default as NoticeCard } from './NoticeCard';
